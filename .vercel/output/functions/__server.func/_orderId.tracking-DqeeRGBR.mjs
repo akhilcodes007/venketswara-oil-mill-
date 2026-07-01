2546e421d8f078e0f1de26c651f30a4d983b5803
SVOM Dev <svem-dev@gmail.com>
2026-07-01 17:38:29 +0530

    fix: remove .vercel/output from git tracking to fix Vercel deployments

--- .vercel/output/functions/__server.func/_orderId.tracking-DqeeRGBR.mjs
+++
@@ -1,6 +0,0 @@
-import { m as createFileRoute, p as lazyRouteComponent } from "./_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/_orderId.tracking-DqeeRGBR.js
-var $$splitComponentImporter = () => import("./_orderId.tracking-DVn6La5t.mjs");
-var Route = createFileRoute("/order/$orderId/tracking")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
--- .vercel/output/functions/__server.func/_ssr/client--XIMi9ld.mjs
+++
@@ -1,5 +0,0 @@
-import { t as createClient } from "../_libs/supabase__supabase-js.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/client--XIMi9ld.js
-var supabase = createClient("https://cuwdevbpxrgugyojinpc.supabase.co", "sb_publishable_DXm_bkl-e-rUYNaYHu0imA_lcgk75K9");
-//#endregion
-export { supabase as t };
